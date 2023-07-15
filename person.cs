4293315d4f6f24f6becd624133052bcb9b2c5d65
Ece ERKCİ <113608214+ECERKCI@users.noreply.github.com>
2023-07-15 15:33:19 +0300

    Add files via upload

---
+++ person.cs
@@ -0,0 +1,11 @@
+namespace Sipay.Bootcamp.ECE_ERKCİ.FluentValidationDemo.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public int AccessLevel { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
