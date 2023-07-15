60e460274312b260b77530fc505960d430c83546
Ece ERKCİ <113608214+ECERKCI@users.noreply.github.com>
2023-07-15 15:33:41 +0300

    Add files via upload

---
+++ PersonController.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Sipay.Bootcamp.ECE_ERKCİ.FluentValidationDemo.Models;
+
+namespace Sipay.Bootcamp.ECE_ERKCİ.FluentValidationDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PersonController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post(Person person)
+    {
+        var validator = new PersonValidator();
+        var validationResult = validator.Validate(person);
+
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.Errors);
+        }
+
+        // Doğrulama başarılı olduğunda burada işlemlerinizi gerçekleştirin.
+
+        return Ok();
+    }
+}
